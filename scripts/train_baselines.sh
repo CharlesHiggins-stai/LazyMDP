fc499a204a568c074de393818d423a5c22647586
CharlesHiggins-stai <charles.higgins@kcl.ac.uk>
2024-06-06 18:11:36 +0100

    adding more structure to pretained policy dir

--- scripts/train_baselines.sh
+++ scripts/train_baselines.sh
@@ -1,6 +1,6 @@
-python baselines/train_ppo_base.py --environment "CartPole-v1" --seed 0 --max_steps 1000000 --output_dir "baselines/pretrained_policies" --env_reward_threshold 500
+python baselines/train_ppo_base.py --environment "CartPole-v1" --seed 0 --max_steps 1000000 --output_dir "baselines/suboptimal_pretrained_policies" --env_reward_threshold 500
 echo "CartPole-v1 done"
-python baselines/train_ppo_base.py --environment "LunarLander-v2" --seed 0 --max_steps 1000000 --output_dir "baselines/pretrained_policies" --env_reward_threshold 200
+python baselines/train_ppo_base.py --environment "LunarLander-v2" --seed 0 --max_steps 1000000 --output_dir "baselines/suboptimal_pretrained_policies" --env_reward_threshold 200
 echo "LunarLander-v2 done"
 # python baselines/train_ppo_base.py --environment "FrozenLake-v1" --seed 0 --max_steps 500000 --output_dir "baselines/pretrained_policies" --env_reward_threshold 200
 # echo "FrozenLake-v1 done"
