dc0cb0f6e09ef992cbb17de99032a68e458f35dd
CharlesHiggins-stai <charles.higgins@kcl.ac.uk>
2024-06-25 15:54:51 +0000

    updated to run across a few seeds for statistical validity

--- scripts/train_lazy_mdp.sh
+++ scripts/train_lazy_mdp.sh
@@ -1,8 +1,9 @@
 # define the fixed variables
-
-for i in $(seq 0 0.1 0.9); do 
-    for j in $(seq 0 2500 10000); do
-    python experiments/lazy_mdp_delayed_start.py --environment "CartPole-v1" --seed 0 --max_steps 1000000 --output_dir "experiments/lazy_mdp_delayed_start_data" --env_reward_threshold 500 --penalty $i --warmup_steps $j --tags "ppo" "un-LazyMDP delayed start"
-    echo "CartPole-v1 done"
-    python experiments/lazy_mdp_delayed_start.py --environment "LunarLander-v2" --seed 0 --max_steps 1000000 --output_dir "experiments/lazy_mdp_delayed_start_data" --env_reward_threshold 200 --penalty $i --warmup_steps $j --tags "ppo" "un-LazyMDP delayed start"
-    echo "LunarLander-v2 done"
+for sd in $(seq 1 1 5); do 
+    for i in $(seq 0 0.1 0.9); do 
+        python experiments/lazy_mdp.py --environment "CartPole-v1" --seed $sd --max_steps 1000000 --output_dir "experiments/lazy_mdp_data" --env_reward_threshold 500 --penalty $i --tags "ppo" "un-LazyMDP"
+        echo "CartPole-v1 done"
+        python experiments/lazy_mdp.py --environment "LunarLander-v2" --seed $sd --max_steps 1000000 --output_dir "experiments/lazy_mdp_data" --env_reward_threshold 200 --penalty $i --tags "ppo" "un-LazyMDP"
+        echo "LunarLander-v2 done"
+    done
+done
