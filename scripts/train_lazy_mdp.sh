8c670c0c6add9d3a3c8d4f695fb8ba1ad75a8758
CharlesHiggins-stai <charles.higgins@kcl.ac.uk>
2024-06-25 15:08:47 +0000

    pushing before a large run to search for hyperparams

--- scripts/train_lazy_mdp.sh
+++ scripts/train_lazy_mdp.sh
@@ -1,11 +1,8 @@
 # define the fixed variables
-output_dir="experiments/pretrained_policies"
-max_steps=1000000
-seed=0
-penalty=0.99
 
-
-python experiments/lazy_mdp.py --environment "CartPole-v1" --seed 0 --max_steps 1000000 --output_dir "experiments/lay_mdp_data" --env_reward_threshold 500 --penalty 0.99
-echo "CartPole-v1 done"
-python experiments/lazy_mdp.py --environment "LunarLander-v2" --seed 0 --max_steps 1000000 --output_dir "experiments/lazy_mdp_data" --env_reward_threshold 200 --penalty 0.99
-echo "LunarLander-v2 done"
+for i in $(seq 0 0.1 0.9); do 
+    for j in $(seq 0 2500 10000); do
+    python experiments/lazy_mdp_delayed_start.py --environment "CartPole-v1" --seed 0 --max_steps 1000000 --output_dir "experiments/lazy_mdp_delayed_start_data" --env_reward_threshold 500 --penalty $i --warmup_steps $j --tags "ppo" "un-LazyMDP delayed start"
+    echo "CartPole-v1 done"
+    python experiments/lazy_mdp_delayed_start.py --environment "LunarLander-v2" --seed 0 --max_steps 1000000 --output_dir "experiments/lazy_mdp_delayed_start_data" --env_reward_threshold 200 --penalty $i --warmup_steps $j --tags "ppo" "un-LazyMDP delayed start"
+    echo "LunarLander-v2 done"
